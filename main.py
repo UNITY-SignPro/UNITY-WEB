42611ce082389d7c35304155206c7f9fac3f421e
Jy7733 <jaeyoung0703@naver.com>
2022-11-07 00:16:12 +0900

    파일생성함수 추가

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
-
 app = FastAPI()
+
+import time
+from pathlib import Path
 
 #root page
 @app.get("/")
@@ -10,3 +12,20 @@ def read_root() :
 @app.get("/translate")
 async def translate(str : str="") :
     return {"hello":str}
+
+# @app.get("/test")
+# def test(str : str="") :
+#     time.sleep(3)
+#     return {"test":str}
+
+def make_file() :
+    Path('newfile.txt').touch()
+    while not (Path('filename.txt').is_file) :
+        time.sleep(1)
+
+    return "newfile.txt"
+
+make_file()
+
+
+
